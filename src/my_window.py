c7678cbff394773a51fd2ef8abfb5c2f9805e2e7
Lei <leiwu05@outlook.com>
2025-10-25 03:19:31 -0400

    feat: compatibility support for Py2

--- src/my_window.py
+++ src/my_window.py
@@ -1,10 +1,14 @@
 # for py2
 from __future__ import unicode_literals, print_function
 
+import sys
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import *
 from maya.OpenMayaUI import MQtUtil
 from shiboken2 import wrapInstance
+
+if sys.version_info.major >= 3:
+    long = int # shim
 
 class MyWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -12,11 +16,15 @@ class MyWindow(QMainWindow):
 
         if not parent:
             # Maya main window's pointer
-            maya_main_ptr = int(MQtUtil.mainWindow())
+            if sys.version_info.major >= 3:
+                maya_main_ptr = int(MQtUtil.mainWindow())
+            else:
+                maya_main_ptr = long(MQtUtil.mainWindow())
+
             # Maya main window
             maya_main = wrapInstance(maya_main_ptr, QWidget)
             # Maya as parent
             self.setParent(maya_main)
 
         # set as floating window, otherwise it's onto maya main window
-        self.setWindowFlag(Qt.Window)
+        self.setWindowFlags(Qt.Window)

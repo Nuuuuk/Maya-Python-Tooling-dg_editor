78dc996212d49c3091d21a1d41fec84d7f87ce76
Lei <leiwu05@outlook.com>
2025-10-25 11:23:48 -0400

    feat: setup ui for tab Nodes

--- src/widget_nodes.py
+++ src/widget_nodes.py
@@ -3,8 +3,75 @@ from __future__ import unicode_literals, print_function
 
 import config
 import sys
+from PySide2.QtGui import *
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import *
+
+class BaseWidget(QWidget):
+    def __init__(self, parent=None):
+        super(BaseWidget, self).__init__(parent)
+
+    # override virtual func, this will auto execute when needed
+    def paintEvent(self, event):
+        p = QPainter(self)
+        p.setPen(Qt.NoPen)
+        p.setBrush(QBrush(QColor(62,62,62)))
+        p.drawRect(self.rect())
+        p.end()
+
+class WidgetA(BaseWidget):
+    def __init__(self, parent=None):
+        super(WidgetA, self).__init__(parent)
+
+        # body_layout
+        self.name_text = QTextEdit()
+        self.create_exp_btn = QPushButton("Create by Exp")
+        self.create_exp_btn.setFixedWidth(170)
+
+        self.create_exp_btn_layout = QVBoxLayout()
+        self.create_exp_btn_layout.addWidget(self.create_exp_btn)
+        self.create_exp_btn_layout.addStretch()
+        self.body_layout = body_layout = QHBoxLayout()
+        body_layout.addWidget(self.name_text)
+        body_layout.addLayout(self.create_exp_btn_layout)
+
+        # main_layout
+        self.create_btn = QPushButton("Create")
+
+        self.main_layout = main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel(text="Create: ", parent=None))
+        main_layout.addLayout(body_layout)
+        main_layout.addWidget(self.create_btn)
+
+        # apply layout to self
+        self.setLayout(main_layout)
+
+class WidgetB(BaseWidget):
+    def __init__(self, parent=None):
+        super(WidgetB, self).__init__(parent)
+
+        # body_layout
+        self.name_text = QTextEdit()
+        self.match_exp_btn = QPushButton("Match by Regular Exp")
+        self.match_exp_btn.setFixedWidth(170)
+
+        self.match_exp_btn_layout = QVBoxLayout()
+        self.match_exp_btn_layout.addWidget(self.match_exp_btn)
+        self.match_exp_btn_layout.addStretch()
+        self.body_layout = body_layout = QHBoxLayout()
+        body_layout.addWidget(self.name_text)
+        body_layout.addLayout(self.match_exp_btn_layout)
+
+        # main_layout
+        self.delete_btn = QPushButton("Delete")
+
+        self.main_layout = main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel(text="Delete: ", parent=None))
+        main_layout.addLayout(body_layout)
+        main_layout.addWidget(self.delete_btn)
+
+        # apply layout to self
+        self.setLayout(main_layout)
 
 class WidgetNodes(QWidget):
     def __init__(self, parent=None):
@@ -12,10 +79,13 @@ class WidgetNodes(QWidget):
 
         # create an empty vertical layout
         self.widget_layout = layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
+        # layout.setContentsMargins(0, 0, 0, 0)
 
-        # add label
-        layout.addWidget(QLabel(text="Nodes", parent=None))
+        self.widget_a = widget_a = WidgetA(self)
+        self.widget_b = widget_b = WidgetB(self)
+
+        layout.addWidget(widget_a)
+        layout.addWidget(widget_b)
 
         #apply layout to widget
         self.setLayout(layout)

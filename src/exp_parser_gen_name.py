f641287d62374a0518ecb431aa71829b4c078886
Lei <leiwu05@outlook.com>
2025-11-27 06:41:49 -0500

    feat: _lex() for name gen

--- src/exp_parser_gen_name.py
+++ src/exp_parser_gen_name.py
@@ -15,6 +15,7 @@ class TokenValue(TokenBase):
     pass
 class GenNameExpExc(Exception):
     pass
+
 name_match = re.compile(r'[a-zA-Z0-9_]+')
 value_match = re.compile(r'\{([a-zA-Z0-9_]+)\}')
 space_match = re.compile(r'[ \t]+')
@@ -24,6 +25,30 @@ def _lex(exp):
     :param exp: abc_{value_name} or abc_{value_name} or abc_{value_name}_def
     :return:
     """
+    while True:
+        if len(exp) == 0:
+            return
+
+        # skip space
+        m = space_match.match(exp)
+        if m is not None:
+            exp = exp[m.end():]
+
+        # match name
+        m = name_match.match(exp)
+        if m is not None:
+            token = TokenName(exp[m.start(): m.end()])
+            exp = exp[m.end():]
+            yield token
+            continue
+
+        # match value
+        m = value_match.match(exp)
+        if m is not None:
+            token = TokenValue(m.group(1))
+            exp = exp[m.end():]
+            yield token
+            continue
 
         raise GenNameExpExc('lex error')
 

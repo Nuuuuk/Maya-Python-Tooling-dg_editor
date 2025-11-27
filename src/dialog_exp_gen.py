f17d2ef38f2f2f982f15e7eeef7cdbc784e8b6ac
Lei <leiwu05@outlook.com>
2025-11-27 07:51:12 -0500

    feat: add node num SpinBox to dialog

--- src/dialog_exp_gen.py
+++ src/dialog_exp_gen.py
@@ -14,15 +14,21 @@ class ExpGenDialog(QDialog):
         self.setWindowTitle("Generate Expression")
         self.main_layout = QHBoxLayout(self)
 
+        self.node_num = QSpinBox(self)
+        self.node_num.setRange(1, 999)
         self.name_line_edit = QLineEdit(self)
         self.type_line_edit = QLineEdit(self)
-        self.run_bn = QPushButton("Generate")
+        self.gen_bn = QPushButton("Generate")
+        self.gen_bn.clicked.connect(self.gen)
+
 
         self.main_layout.addWidget(QLabel("Name: "))
         self.main_layout.addWidget(self.name_line_edit)
         self.main_layout.addWidget(QLabel("Type: "))
         self.main_layout.addWidget(self.type_line_edit)
-        self.main_layout.addWidget(self.run_bn)
+        self.main_layout.addWidget(QLabel("Node Num: "))
+        self.main_layout.addWidget(self.node_num)
+        self.main_layout.addWidget(self.gen_bn)
 
 
 def exec_(parent=None):

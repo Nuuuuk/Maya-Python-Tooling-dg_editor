eebf73211035b87b33f6951fdfc6f24ddafa6746
Lei <leiwu05@outlook.com>
2025-11-28 05:40:01 -0500

    feat: in and out attribute matches layout

--- src/ui_tab_connections.py
+++ src/ui_tab_connections.py
@@ -13,11 +13,11 @@ class WidgetFuncSelect(QWidget):
 
     def __init__(self, parent=None):
         super(WidgetFuncSelect, self).__init__(parent)
-        self.main_layout = layout = QHBoxLayout()
         self.func_combo = QComboBox()
         self.func_combo.addItem(self.ConnectText)
         self.func_combo.addItem(self.DisconnectText)
 
+        self.main_layout = layout = QHBoxLayout()
         layout.addWidget(QLabel('Function:'))
         layout.addWidget(self.func_combo)
         layout.addStretch(0)
@@ -30,6 +30,25 @@ class WidgetFuncSelect(QWidget):
         else:
             return self.Disconnect
 
+class MatchWidget(QWidget):
+    def __init__(self, parent=None):
+        super(MatchWidget, self).__init__(parent)
+        self.text_box = QTextEdit()
+        self.match_exp_btn = QPushButton("Match by Regex")
+        self.match_exp_btn.setFixedWidth(170)
+
+        self.match_exp_btn_layout = QVBoxLayout()
+        self.match_exp_btn_layout.addWidget(self.match_exp_btn)
+        self.match_exp_btn_layout.addStretch()
+        self.body_layout = body_layout = QHBoxLayout()
+        body_layout.addWidget(self.text_box)
+        body_layout.addLayout(self.match_exp_btn_layout)
+
+        self.setLayout(body_layout)
+
+    def get_names(self):
+        return self.text_box.toPlainText().splitlines()
+
 class WidgetConnections(QWidget):
     def __init__(self, parent=None):
         super(WidgetConnections, self).__init__(parent)
@@ -39,6 +58,12 @@ class WidgetConnections(QWidget):
         # layout.setContentsMargins(0, 0, 0, 0)
         self.widget_func_select = widget_func_select = WidgetFuncSelect(self)
         layout.addWidget(widget_func_select)
+        self.widget_match_out_attrs = MatchWidget(self)
+        self.widget_match_in_attrs = MatchWidget(self)
+        layout.addWidget(QLabel('Out Attributes:'))
+        layout.addWidget(self.widget_match_out_attrs)
+        layout.addWidget(QLabel('In Attributes:'))
+        layout.addWidget(self.widget_match_in_attrs)
 
         #apply layout to widget
         self.setLayout(layout)

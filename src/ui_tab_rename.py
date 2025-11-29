33fe1562f36ab4aa3c9e59e966c3a2ee33237b46
Lei <leiwu05@outlook.com>
2025-11-29 01:13:43 -0500

    feat: base widget class; prefix and replace sub classes

--- src/ui_tab_rename.py
+++ src/ui_tab_rename.py
@@ -11,7 +11,34 @@ from PySide2.QtGui import *
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import *
 
-import maya.cmds as cmds
+class BaseWidget(QWidget):
+    def __init__(self, parent=None):
+        super(BaseWidget, self).__init__(parent)
+        self.main_layout = QVBoxLayout(self)
+
+    # a faint background to distinguish two widgets
+    def paintEvent(self, event):
+        p = QPainter(self)
+        p.setPen(Qt.NoPen)
+        p.setBrush(QBrush(QColor(62,62,62)))
+        p.drawRect(self.rect())
+        p.end()
+
+
+class WidgetPrefix(BaseWidget):
+    def __init__(self, parent=None):
+        super(WidgetPrefix, self).__init__(parent)
+        self.body_layout = QHBoxLayout()
+        self.main_layout.addWidget(QLabel("Add Prefix"))
+
+
+class WidgetReplace(BaseWidget):
+    def __init__(self, parent=None):
+        super(WidgetReplace, self).__init__(parent)
+        self.search_layout = search_layout = QHBoxLayout()
+        self.main_layout.addWidget(QLabel("Search and Replace"))
+
+        self.main_layout.addStretch()
 
 
 class WidgetRename(QWidget):
@@ -22,11 +49,11 @@ class WidgetRename(QWidget):
         self.widget_layout = layout = QVBoxLayout()
         # layout.setContentsMargins(0, 0, 0, 0)
 
-        # self.widget_a = widget_a = WidgetCreate(self)
-        # self.widget_b = widget_b = WidgetDelete(self)
-        #
-        # layout.addWidget(widget_a)
-        # layout.addWidget(widget_b)
+        self.widget_a = widget_a = WidgetPrefix(self)
+        self.widget_b = widget_b = WidgetReplace(self)
+
+        layout.addWidget(widget_a)
+        layout.addWidget(widget_b)
 
         #apply layout to widget
         self.setLayout(layout)

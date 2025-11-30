1d246040af345bbab8c0f393a678f8e853138456
Lei <leiwu05@outlook.com>
2025-11-29 21:14:40 -0500

    feat: connect font size function in all three dialogs

--- src/ui_nodes_delete_dialog.py
+++ src/ui_nodes_delete_dialog.py
@@ -8,6 +8,7 @@ from __future__ import unicode_literals, print_function
 from PySide2.QtWidgets import *
 
 import nodes_delete_dialog
+import settings
 
 class DelMatchDialog(QDialog):
     def __init__(self, parent=None):
@@ -24,6 +25,15 @@ class DelMatchDialog(QDialog):
         self.main_layout.addWidget(QLabel("Regex: "))
         self.main_layout.addWidget(self.name_line_edit)
         self.main_layout.addWidget(self.match_bn)
+
+        # apply font size
+        self.set_font_size()
+
+    def set_font_size(self):
+        font_size = settings.get('font_size')
+        font = self.font()
+        font.setPointSize(font_size)
+        self.setFont(font)
 
     def gen(self):
         self.names = list(nodes_delete_dialog.get_matched_nodes(self.name_line_edit.text()))

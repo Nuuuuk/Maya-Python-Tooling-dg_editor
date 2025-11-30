a301e50e1d4272253499769270b454dd1429a838
Lei <leiwu05@outlook.com>
2025-11-29 20:05:41 -0500

    feat: read/save current settings from/as json

---
+++ src/settings.py
@@ -0,0 +1,30 @@
+# for py2
+from __future__ import unicode_literals, print_function
+
+import json, os
+import codecs
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(PATH, "settings.json")
+
+def _get_json():
+    if not os.path.isfile(JSON_PATH):
+        _save_json(dict())
+    with codecs.open(JSON_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def _save_json(data):
+    with codecs.open(JSON_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def set(key, val):
+    data = _get_json()
+    data[key] = val
+    _save_json(data)
+
+
+def get(key, default):
+    data = _get_json()
+    return data.get(key, default)

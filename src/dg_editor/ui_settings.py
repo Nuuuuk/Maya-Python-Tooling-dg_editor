d476db6dd546309761b0cb59d26ec5d382fac554
Lei <leiwu05@outlook.com>
2025-12-01 05:18:59 -0500

    refactor: 'ui_settings.py': use binding functions and remove redundant getter/setter methods

--- src/dg_editor/ui_settings.py
+++ src/dg_editor/ui_settings.py
@@ -16,16 +16,9 @@ class UndoWidget(BaseWidget):
         super(UndoWidget, self).__init__(parent)
         self.undo_check = QCheckBox()
 
-        self.undo_check.setChecked(self.get_undo_check())
-        self.undo_check.toggled.connect(lambda *args: self.set_undo_check())
+        settings.bind_checkbox(self.undo_check, "settings_undo_check", True)
 
         self.add_input_row("Bulk Undo", self.undo_check, False)
-
-    def get_undo_check(self):
-        return settings.get_undo_check()
-
-    def set_undo_check(self):
-        settings.set("undo_check", self.undo_check.isChecked())
 
 
 class FontSizeWidget(BaseWidget):
@@ -34,16 +27,9 @@ class FontSizeWidget(BaseWidget):
         self.font_size_input = QSpinBox()
         self.font_size_input.setRange(5, 15)
 
-        self.font_size_input.setValue(self.get_font_size())
-        self.font_size_input.valueChanged.connect(lambda *args: self.set_font_size())
+        settings.bind_spinbox(self.font_size_input, "settings_font_size", 10)
 
         self.add_input_row("Font Size", self.font_size_input, False)
-
-    def get_font_size(self):
-        return settings.get_font_size()
-
-    def set_font_size(self):
-        settings.set("font_size", self.font_size_input.value())
 
 
 class SettingsWidget(QWidget):
